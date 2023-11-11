e0725b15c3200a01ef9bc28c52295df7adcd95fb
Theo Madzou <madztheo@hotmail.fr>
2023-11-11 23:58:56 +0300

    small metadata changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.scss";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Catz",
+  title: "Katz",
   description: "No time to get catfished",
 };
 
